06138112ef14f6eda4abc7a68717ffe01cc73a43
SangojuSrijaRaj <sangojusrijaraj35@gmail.com>
2026-03-13 11:50:05 +0530

    update code

---
+++ routes/orderRoute.js
@@ -0,0 +1,9 @@
+
+import { placeOrder,showOrders } from "../controllers/orderController.js";
+import express from "express"
+const orderRouter = express.Router()
+
+orderRouter.post("/",placeOrder)
+orderRouter.get("/:email",showOrders)
+
+export default orderRouter
